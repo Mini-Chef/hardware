552de57a68e37a6f302864be606618022b749352
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-13 22:07:02 -0300

    bug-fix: dhtInstance scope fix

--- Firmware/mini-chef-firmware/lib/dht_sensor/dht_sensor.cpp
+++ Firmware/mini-chef-firmware/lib/dht_sensor/dht_sensor.cpp
@@ -1,44 +1,15 @@
 #include "dht_sensor.h"
 
-/*
-
-    
-    class dhtSensor {
-    public:
-        dhtSensor(uint8_t pinValue = DHTPIN)
-        {
-            pin = pinValue;
-        }
-        // Getter
-        void getHumidity(float* value){
-            setHumidity();
-            *value = humidity;
-        }
-        void getTemperature(float* value){
-            setTemperature(&temperature);
-            *value = temperature;
-            Serial.println(temperature);
-        }
-    private:
-        void setTemperature(float* value){
-            temperature = dhtInstanse.readTemperature();
-
-        }
-        void setHumidity(){
-            humidity = dhtInstanse.readHumidity();
-        }
-};
-*/
-dhtSensor::dhtSensor(uint8_t pinValue = DHTPIN){
+dhtSensor::dhtSensor(uint8_t pinValue){
     pin = pinValue;
 }
 
 void dhtSensor::setTemperature(){
-    temperature = dhtInstanse.readTemperature();
+    temperature = dhtInstance.readTemperature();
 }
 
 void dhtSensor::setHumidity(){
-    humidity = dhtInstanse.readHumidity();
+    humidity = dhtInstance.readHumidity();
 }
 
 void dhtSensor::getTemperature(float* value){
--- Firmware/mini-chef-firmware/lib/dht_sensor/dht_sensor.h
+++ Firmware/mini-chef-firmware/lib/dht_sensor/dht_sensor.h
@@ -1,28 +1,21 @@
+#include <DHT.h>
+
 #ifndef DHT_SENSOR_H
 #define DHT_SENSOR_H
-
-#include "DHT.h"
-
 #define DHTPIN 18
 #define DHTTYPE DHT11
-
-    uint8_t pin;
-    float humidity = 0;
-    float temperature = 0;
-    DHT dhtInstanse(DHTPIN, DHTTYPE);
-    // Setter
     
-    
-    class dhtSensor {
+    class dhtSensor{
         public:
             dhtSensor(uint8_t pinValue = DHTPIN);
-            // Getter
             void getHumidity(float* value);
             void getTemperature(float* value);
         private:
+            uint8_t pin = 0;
+            float humidity = 0;
+            float temperature = 0;
+            DHT dhtInstance = DHT(DHTPIN, DHTTYPE);
             void setTemperature();
             void setHumidity();
     };
-
-
 #endif
