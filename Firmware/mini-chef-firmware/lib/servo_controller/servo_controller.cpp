4a3114a314a3cdf100c1bb24d0a2cfa829ec7bf7
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-14 20:16:51 -0300

    bug-fix: int declaration duplate

--- Firmware/mini-chef-firmware/lib/servo_controller/servo_controller.cpp
+++ Firmware/mini-chef-firmware/lib/servo_controller/servo_controller.cpp
@@ -1,6 +1,6 @@
 #include "servo_controller.h"
 
-servoController::servoController(uint8_t pinValue = servoDefaultPin){
+servoController::servoController(uint8_t pinValue){
     setServoPin( pinValue );
     closeFeeder();
 }
@@ -9,12 +9,12 @@ void servoController::setServoPin(uint8_t pin){
     servo.attach( pin );
 }
 
-void servoController::openLeft(int milliseconds = 350){
+void servoController::openLeft(int milliseconds){
     servo.write(170);
     delay(milliseconds);
 }
 
-void servoController::openRight(int milliseconds = 350){
+void servoController::openRight(int milliseconds){
     servo.write(0);
     delay(milliseconds);
 }
