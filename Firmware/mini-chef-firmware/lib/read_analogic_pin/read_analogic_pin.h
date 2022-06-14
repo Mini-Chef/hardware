b841f96339efebe73f56fc032e58fbe2a617f8cf
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-13 22:11:11 -0300

    refac: refactoring readAnalog lib to c++ OOP

--- Firmware/mini-chef-firmware/lib/read_analogic_pin/read_analogic_pin.h
+++ Firmware/mini-chef-firmware/lib/read_analogic_pin/read_analogic_pin.h
@@ -2,50 +2,26 @@
 
 #ifndef READ_ANALOGIC_PIN_H
 #define READ_ANALOGIC_PIN_H
+
 #define THRESHOLD_LOW 1175
 #define THRESHOLD_HIGH 2413
 #define CONFIDENCE 500
-
-    uint8_t defaultPin = 34;
 
     class readAnalogicPin {
         private:
             uint8_t pin;
             uint16_t read = -1;
-
-            void setRead(uint16_t* value){
-                read = *value;
-            }
-            void setPin(uint8_t Pin){
-                pin = Pin;
-            }
+            static const uint8_t defaultPin = 34;
+            
+            void setRead(uint16_t* value);
+            void setPin(uint8_t* Pin);
         public:
             // Constructor
-            readAnalogicPin(uint8_t Pin = defaultPin){
-                pin = Pin;
-            }
-            
-            void readPin(uint16_t* value = 0){
-                uint16_t tmpRead = analogRead(pin);
-                setRead(&tmpRead);
-                *value = tmpRead;
-            }
-            void refreshPin(){
-                uint16_t temp = analogRead(pin);
-                setRead(&temp);
-                
-            }
-            boolean isFull(uint16_t* value){
-                if (*value <= (THRESHOLD_HIGH - CONFIDENCE)){
-                    return 1;
-                }else{
-                    return 0;
-                }
-            }
-            // Getters
-            void getRead(uint16_t* value){
-                *value = read;
-            }
+            readAnalogicPin(uint8_t Pin = defaultPin);
+            void readPin(uint16_t* value);
+            void refreshPin();
+            void getRead(uint16_t* value);
+            boolean isFull(uint16_t* value);
     };
 
 #endif
